ae8d7ffeda5533396016d26cb02ef91909e5cbea
cqbjk <claudio@quabblejack.com>
2015-07-22 14:21:24 -0400

    added rotate. works once.

--- sunday/ViewController.swift
+++ sunday/ViewController.swift
@@ -9,17 +9,89 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var squareView: UIView!
+    @IBOutlet var opacitySlider: UISlider!
+    @IBOutlet var redButton: UIButton!
+    @IBOutlet var greenButton: UIButton!
+    @IBOutlet var blueButton: UIButton!
+    @IBOutlet var rotateButton: UIButton!
+    
+    var currentColor: CurrentColor = .Red
 
+    // could do CurrentColor.Red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setDefaultColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
+        
     }
 
+    @IBAction func updateColor(){
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        let opacity = CGFloat(opacitySlider.value)
+        
+        squareView.backgroundColor = UIColor(red: red, green:green, blue:blue, alpha: opacity)
+    
+    }
+    
+    @IBAction func setRed(sender: AnyObject) {
+        currentColor = .Red
+        setDefaultColor()
+    }
+    @IBAction func setGreen(sender: AnyObject) {
+        currentColor = .Green
+        setDefaultColor()
+    }
+    @IBAction func setBlue(sender: AnyObject) {
+        currentColor = .Blue
+        setDefaultColor()
+    }
+    
+    @IBAction func rotateImage(sender: UIButton) {
+        UIView.animateWithDuration(2.0, animations: {
+            self.squareView.transform = CGAffineTransformMakeRotation((180.0 * CGFloat(M_PI)) / 180.0)
+        })
+    }
+    
+    func setDefaultColor(){
+        switch currentColor{
+        case .Red:
+            redSlider.value = 1
+            greenSlider.value = 0
+            blueSlider.value = 0
+        case .Green:
+            redSlider.value = 0
+            greenSlider.value = 1
+            blueSlider.value = 0
+        case .Blue:
+            redSlider.value = 0
+            greenSlider.value = 0
+            blueSlider.value = 1
+        }
+        updateColor()
+    }
+    
+}
 
+enum CurrentColor{
+    case Red
+    case Green
+    case Blue
 }
 
